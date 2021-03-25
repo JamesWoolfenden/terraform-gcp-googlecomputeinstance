f45763bb5b3707565573cf5afb0d3bbcc8bee8a2
James Woolfenden <James.Woolfenden@gmail.com>
2021-03-25 09:54:05 +0000

    wip

--- google_compute_instance.vm_instance.tf
+++ google_compute_instance.vm_instance.tf
@@ -1,7 +1,8 @@
 resource "google_compute_instance" "vm_instance" {
-  #checkov:skip=CKV_GCP_32: "Ensure 'Block Project-wide SSH keys' is enabled for VM instances"
-  #checkov:skip=CKV_GCP_39: "Ensure Compute instances are launched with Shielded VM enabled"
-  #checkov:skip=CKV_GCP_30: "Ensure that instances are not configured to use the default service account"
+  # checkov:skip=CKV_GCP_38: "todo add csek"
+  # checkov:skip=CKV_GCP_32: "Ensure 'Block Project-wide SSH keys' is enabled for VM instances"
+  # checkov:skip=CKV_GCP_39: "Ensure Compute instances are launched with Shielded VM enabled"
+  # checkov:skip=CKV_GCP_30: "Ensure that instances are not configured to use the default service account"
   name         = "terraform-instance"
   project      = var.project_id
   machine_type = var.machine_type
